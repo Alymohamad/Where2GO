bbe8d37b6be492ddec6443b7f0f9f9b7d246da47
Mohamad Aly <elzaidy@Mohamads-MacBook-Pro.local>
2021-02-11 20:47:41 +0100

    Initial Commit Spring boot App

---
+++ src/main/java/com/example/springboot/Where2GoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Where2GoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Where2GoApplication.class, args);
+	}
+
+}
