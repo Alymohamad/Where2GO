c11ce577ba6b101b3d89fe3b008a903827535c37
DonAntonio22 <antonio.lastro@outlook.com>
2021-02-14 00:46:53 +0100

    adding gmap as google maps window, sending city, filter and radius

--- frontend/Where2GO/src/app/app.module.ts
+++ frontend/Where2GO/src/app/app.module.ts
@@ -26,7 +26,6 @@ import { NewSearchComponent } from './components/new-search/new-search.component
 import { FormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { MatSliderModule } from '@angular/material/slider';
-import { MapResult2Component } from './components/map-result2/map-result2.component';
 import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
@@ -35,7 +34,6 @@ import { AgmCoreModule } from '@agm/core';
     NavigationSiteComponent,
     MapResultComponent,
     NewSearchComponent,
-    MapResult2Component
   ],
   imports: [
     BrowserModule,
@@ -63,7 +61,7 @@ import { AgmCoreModule } from '@agm/core';
     MatSliderModule,
     GoogleMapsModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyBmIO594wcxzhjsoGiSHfIDzJifq5wT4kA'
+      apiKey: ''
     }),
   ],
   exports: [
